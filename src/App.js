25691c3b3e6fb3d9393f04aef525083ff8bfce53
thoabk98 <hoangthithoa09041998@gmail.com>
2020-04-05 18:33:57 +0700

    init app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Row, Col, Tabs, Tab} from 'react-bootstrap'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      activeTab: 'data',
+      data: [],
+      world: {
+        cases: 0,
+        deaths: 0,
+        recovered: 0,
+        critical: 0,
+        updated: 0
+      }
+    }
+    this.handleSelect = this.handleSelect.bind(this)
+    this.numberWithCommas = this.numberWithCommas.bind(this)
+  }
+  handleSelect(selectedTab){
+    this.setState({
+      activeTab: selectedTab
+    });
+  }
+  numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+  }
+  componentWillMount(){
+    axios.get('https://corona.lmao.ninja/countries?sort=cases')
+         .then(res => {
+           this.setState({ data: res.data, world: res.data[0] })
+         })
+  }
+  render(){
+    return(
+      <div className="App">
+        <Container fluid>
+          <Row>
+            <Col md={4} className="info sticky-top">
+              
+            </Col>
+            <Col md={8}>
+              <Tabs defaultActiveKey="data" onSelect={this.handleSelect}>
+                <Tab eventKey="data" title="感染情報">
+                  < div className="data-table">
+                  
+                  </div>
+                </Tab>
+                <Tab eventKey="chart" title="グラフ">
+                
+                </Tab>
+                <Tab eventKey="news" title="ニュース">
+                 
+                </Tab>               
+              </Tabs>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    )
+  }
 }
 
 export default App;
